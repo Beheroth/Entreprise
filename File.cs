1a18ac99652466b5b7f8aee220fb9ec722587859
2a6m <maxime.bourguignon1996@gmail.com>
2017-10-22 20:49:59 +0200

    Create Class Generator

--- File.cs
+++ File.cs
@@ -25,54 +25,14 @@ namespace Entreprise
             return lines;
         }
 
+        public string[] Load 
+        {
+            get {return this.LoadFile();}
+        }
+
         private void SaveFile(String text)
         {
             System.IO.File.WriteAllText(this.Path, text);
-        }
-
-        public Dictionary<String, List<List<String>>> LoadEmployes()
-        {
-            // Load the file and putt each line in a Table of string
-            Dictionary<String, List<List<String>>> result = new Dictionary<String, List<List<String>>>();
-            string[] lines = this.LoadFile();
-            foreach (string line in lines)
-            {
-                // Match the lines with a regex to extract the information and stock it in a dictionnary
-                Regex rg = new Regex(@"^(?<job>\w+)-(?<firstname>\w+)-(?<lastname>\w+)$");
-                Match m = rg.Match(line);
-                if (m.Success)
-                {
-                    List<String> name = new List<String>();
-                    name.Add(m.Groups["firstname"].Value);
-                    name.Add(m.Groups["lastname"].Value);
-
-                    // Check if the key exist in the Dictionnary
-                    if (result.ContainsKey(m.Groups["job"].Value))
-                    {
-                        result[m.Groups["job"].Value].Add(name);
-                    }
-
-                    else
-                    {
-                        List<List<String>> value = new List<List<String>>();
-                        result[m.Groups["job"].Value] = value;
-                        result[m.Groups["job"].Value].Add(name);
-                    }
-                }
-            }
-            return result;
-        }
-
-        public void SaveEmployes(List<List<String>> employes)
-        {
-            String text = "File with all the employes";
-            text += Environment.NewLine;
-            foreach(List<String> employe in employes)
-            {
-                text += employe[0] + '-' + employe[1] + '-' + employe[2];
-                text += Environment.NewLine;
-            }
-            this.SaveFile(text);
         }
     }
 }
---
+++ Generator.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Entreprise
+{
+    class Generator
+    {
+        public Generator()
+        {
+        }
+
+        // Method to generate the lists of the entreprise
+
+        public List<Consultant> GenerateConsultant(String filename)
+        {
+            List<Consultant> result = new List<Consultant>();
+            File consultantfile = new File(filename);
+            // Extract each line of the file 
+            foreach (string c in consultantfile.Load)
+            {
+                // Use the lines who match the pattern of the regex
+                Regex rg = new Regex(@"^(?<job>\[a-zA-Z])/(?<firstname>[a-zA-Z])/(?<lastname>[a-zA-Z])/(?<personalaccount>[0-9])$");
+                Match m = rg.Match(c);
+                if (m.Success)
+                {
+                    if (m.Groups["job"].Value == "consultant")
+                    {
+                        // Generate the consultant
+                        int pa = Int32.Parse(m.Groups["personalaccount"].Value);
+                        Consultant consultant = new Consultant(m.Groups["firstname"].Value, m.Groups["lastname"].Value, pa);
+                        result.Add(consultant);
+                    }
+                }
+            }
+            return result;
+        }
+
+        public List<Director> GenerateDirector(String filename)
+        {
+            List<Director> result = new List<Director>();
+            File directorfile = new File(filename);
+            // Extract each line of the file
+            foreach (string c in directorfile.Load)
+            {
+                // Use the lines who match the pattern of the regex
+                Regex rg = new Regex(@"^(?<job>\[a-zA-Z])/(?<firstname>[a-zA-Z])/(?<lastname>[a-zA-Z])/(?<personalaccount>[0-9])$");
+                Match m = rg.Match(c);
+                if (m.Success)
+                {
+                    // Generate the directors
+                    if (m.Groups["job"].Value == "director")
+                    {
+                        int pa = Int32.Parse(m.Groups["personalaccount"].Value);
+                        Director director = new Director(m.Groups["firstname"].Value, m.Groups["lastname"].Value, pa);
+                        result.Add(director);
+                    }
+                    if (m.Groups["job"].Value == "financialdirector")
+                    {
+                        int pa = Int32.Parse(m.Groups["personalaccount"].Value);
+                        FinancialDirector director = new FinancialDirector(m.Groups["firstname"].Value, m.Groups["lastname"].Value, pa);
+                        result.Add(director);
+                    }
+                    if(m.Groups["job"].Value == "humanresourcesdirector")
+                    {
+                        int pa = Int32.Parse(m.Groups["personalaccount"].Value);
+                        HumanResourcesDirector director = new HumanResourcesDirector(m.Groups["firstname"].Value, m.Groups["lastname"].Value, pa);
+                        result.Add(director);
+                    }
+                }
+            }
+            return result;
+        }
+        public List<Manager> GenerateManager(String filename)
+        {
+            List<Manager> result = new List<Manager>();
+            File managerfile = new File(filename);
+            // Extract each line of the file
+            foreach (string c in managerfile.Load)
+            {
+                // Use the lines who match the pattern of the regex
+                Regex rg = new Regex(@"^(?<job>\[a-zA-Z])/(?<firstname>[a-zA-Z])/(?<lastname>[a-zA-Z])/(?<personalaccount>[0-9])/(?<consultantslist>\S)$");
+                Match m = rg.Match(c);
+                if (m.Success)
+                {
+                    if (m.Groups["job"].Value == "manager")
+                    {
+                        // Generate the manager
+                        int pa = Int32.Parse(m.Groups["personalaccount"].Value);
+                        Manager manager = new Manager(m.Groups["firstname"].Value, m.Groups["lastname"].Value, pa);
+
+                        foreach (string consultant in m.Groups["consultantslist"].Value.Split('.'))
+                        {
+                            // must receive the dico of all the consultants to link them at their manager or
+                        }
+
+                        manager.AddConsultant(consultant);
+                        result.Add(manager);
+                    }
+                }
+            }
+            return result;
+
+        }
+
+        public List<Client> GenerateClient(String filename)
+        {
+            List<Client> result = new List<Client>();
+            File clientfile = new File(filename);
+            // Extract each line of the file
+            foreach (string c in clientfile.Load)
+            {
+                Regex rg = new Regex(@"^(?<job>[a-zA-Z])/(?<name>\w)$");
+                Match m = rg.Match(c);
+                if (m.Success)
+                {
+                    if (m.Groups["job"].Value == "client")
+                    {
+                        // Generate the client
+                        Client client = new Client(m.Groups["name"].Value);
+                        result.Add(client);
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
