ab03dc5246251f0f93fed885723126371e6c1a06
2a6m <maxime.bourguignon1996@gmail.com>
2017-10-22 14:20:10 +0200

    add Class File

---
+++ File.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entreprise
+{
+    class File
+    {
+        public String FileName;
+        public String Path;
+
+        public File(String filename)
+        {
+            this.FileName = filename;
+            this.Path = @"\Data" + this.FileName;
+
+        }
+    }
+}
