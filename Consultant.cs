03137b2d1a7d10e890b517a838b223f1bf36fff4
Beheroth <arnaud.peeters95@gmail.com>
2017-10-24 10:00:39 +0200

    Added MissionHistory Attribute

--- Consultant.cs
+++ Consultant.cs
@@ -9,30 +9,50 @@ namespace Entreprise
     class Consultant : Person
     {
         private List<Mission> Missionagenda;
+        private List<Mission> MissionHistory; //Sorted List of All the missions the consultant has done or is doing.
 
         public Consultant(String firstname, String lastname, int personnalaccount) : base(firstname, lastname, personnalaccount)
         {
             this.Missionagenda = new List<Mission>();
+            this.MissionHistory = new List<Mission>();
         }
 
         //getter-setter
+
+                //BEWARE OF SHALLOW COPIES!
 
         public List<Mission> GetMissionagenda()
         {
             return this.Missionagenda;
         }
 
-        public void SetMissionagenda(List<Mission> foo)
+        public List<Mission> GetMissionHistory()
         {
-            this.Missionagenda = foo;
+            return this.MissionHistory;
         }
+
+        public void SetMissionagenda(List<Mission> var)
+        {
+            //assert that foo is of length 1
+            this.Missionagenda = var;
+        }
+
+        public void SetMissionHistory(List<Mission> var)
+        {
+            this.MissionHistory = var;
+            this.MissionHistory.Sort();
+        }
+
 
         //methods
 
-        public void AddMission(Mission Mission)
+        public void AddMission(Mission mission)
         {
             //assert that mission agenda is empty
-            this.Missionagenda.Add(Mission);
+            this.Missionagenda.Add(mission);
+            //assert that mission is not overlapping with any mission in missionhistory
+            this.MissionHistory.Add(mission);
+            this.MissionHistory.Sort();
         }
 
         public void ClearMissionagenda()
@@ -41,6 +61,7 @@ namespace Entreprise
         }
 
         public void CheckIsBusy(DateTime date)
+            //updates the Missionagenda list if the given date is after the end of the mission in missionagenda
         {
             bool ans = false;
             if(this.Missionagenda.Count == 1)
